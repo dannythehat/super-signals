f8f0a75beac3731f3a6359d210285ab3f6701d7c
dannythehat <dannythehat2@gmail.com>
2026-08-13 12:14:49 +0300

    Register Day 36 manual MT5 activity route

--- services/api/app/routes/__init__.py
+++ services/api/app/routes/__init__.py
@@ -7,6 +7,7 @@ routers, keeping invitation and invited-user controls isolated from startup code
 from . import admin_accounts as _admin_accounts  # noqa: F401
 from . import auth as _auth  # noqa: F401
 from . import invitations as _invitations  # noqa: F401
+from . import manual_reconciliation_day36 as _manual_reconciliation_day36
 from . import mt5_approvals_day30 as _mt5_approvals_day30
 from . import mt5_onboarding_day35 as _mt5_onboarding_day35
 from . import registration as _registration  # noqa: F401
@@ -15,4 +16,5 @@ from . import user_mt5_accounts as _user_mt5_accounts
 
 _user_mt5_accounts.router.include_router(_trading_controls_day31.router)
 _user_mt5_accounts.router.include_router(_mt5_onboarding_day35.user_router)
+_user_mt5_accounts.router.include_router(_manual_reconciliation_day36.router)
 _mt5_approvals_day30.router.include_router(_mt5_onboarding_day35.owner_router)
