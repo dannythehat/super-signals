959b191b0030b973ac237dd18dbe4f893eb1bd55
dannythehat <dannythehat2@gmail.com>
2026-08-18 20:31:40 +0300

    Install Aug 18 trade capture fixes

--- services/api/app/__init__.py
+++ services/api/app/__init__.py
@@ -89,3 +89,7 @@ install_performance_account_truth_override()
 from app.account_truth_poll_override import install_account_truth_poll_override
 
 install_account_truth_poll_override()
+
+from app.aug18_trade_capture_overrides import install_aug18_trade_capture_overrides
+
+install_aug18_trade_capture_overrides()
