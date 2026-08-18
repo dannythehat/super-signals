e405010cf0790deda307a03851be75c7a3638323
dannythehat <dannythehat2@gmail.com>
2026-08-18 20:50:39 +0300

    Install Aug 18 reporting readiness cleanup

--- services/api/app/__init__.py
+++ services/api/app/__init__.py
@@ -99,3 +99,7 @@ install_ambiguous_trade_reconciliation_override()
 from app.aug18_trade_capture_overrides import install_aug18_trade_capture_overrides
 
 install_aug18_trade_capture_overrides()
+
+from app.aug18_readiness_cleanup import install_aug18_readiness_cleanup
+
+install_aug18_readiness_cleanup()
