3fb631e3ae2ee2463a286140cfeaa9ce1379e9d2
dannythehat <dannythehat2@gmail.com>
2026-08-30 16:38:18 +0300

    Register subscription member and owner routes

--- services/api/app/routes/__init__.py
+++ services/api/app/routes/__init__.py
@@ -13,11 +13,14 @@ from . import mt5_onboarding_day35 as _mt5_onboarding_day35
 from . import owner_manual_close as _owner_manual_close
 from . import public_registration as _public_registration  # noqa: F401
 from . import registration as _registration  # noqa: F401
+from . import subscriptions as _subscriptions
 from . import trading_controls_day31 as _trading_controls_day31
 from . import user_mt5_accounts as _user_mt5_accounts
 
+_user_mt5_accounts.router.include_router(_subscriptions.user_router)
 _user_mt5_accounts.router.include_router(_trading_controls_day31.router)
 _user_mt5_accounts.router.include_router(_mt5_onboarding_day35.user_router)
 _user_mt5_accounts.router.include_router(_manual_reconciliation_day36.router)
 _user_mt5_accounts.router.include_router(_owner_manual_close.router)
 _mt5_approvals_day30.router.include_router(_mt5_onboarding_day35.owner_router)
+_mt5_approvals_day30.router.include_router(_subscriptions.owner_router)
