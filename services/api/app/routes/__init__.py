873c4cb669e92160a4801196125f724b3383d66c
dannythehat <dannythehat2@gmail.com>
2026-08-30 16:59:53 +0300

    Register complimentary access owner route

--- services/api/app/routes/__init__.py
+++ services/api/app/routes/__init__.py
@@ -6,6 +6,7 @@ routers, keeping invitation and invited-user controls isolated from startup code
 
 from . import admin_accounts as _admin_accounts  # noqa: F401
 from . import auth as _auth  # noqa: F401
+from . import complimentary_access as _complimentary_access
 from . import invitations as _invitations  # noqa: F401
 from . import manual_reconciliation_day36 as _manual_reconciliation_day36
 from . import mt5_approvals_day30 as _mt5_approvals_day30
@@ -17,6 +18,7 @@ from . import subscriptions as _subscriptions
 from . import trading_controls_day31 as _trading_controls_day31
 from . import user_mt5_accounts as _user_mt5_accounts
 
+_subscriptions.owner_router.include_router(_complimentary_access.router)
 _user_mt5_accounts.router.include_router(_subscriptions.user_router)
 _user_mt5_accounts.router.include_router(_trading_controls_day31.router)
 _user_mt5_accounts.router.include_router(_mt5_onboarding_day35.user_router)
