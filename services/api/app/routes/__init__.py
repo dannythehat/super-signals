3d77952abe48fa894a7c9899c3afb3dfef6e4d83
dannythehat <dannythehat2@gmail.com>
2026-09-14 08:32:33 +0300

    Register rebuilt universal MT5 onboarding router explicitly

--- services/api/app/routes/__init__.py
+++ services/api/app/routes/__init__.py
@@ -10,7 +10,7 @@ from . import auth as _auth  # noqa: F401
 from . import complimentary_access as _complimentary_access
 from . import invitations as _invitations  # noqa: F401
 from . import manual_reconciliation_day36 as _manual_reconciliation_day36
-from . import member_connection_v2 as _member_connection_v2  # noqa: F401
+from . import member_connection_v2 as _member_connection_v2
 from . import member_onboarding_owner as _member_onboarding_owner  # noqa: F401
 from . import member_subscription_owner_hotfix as _member_subscription_owner_hotfix
 from . import mt5_approvals_day30 as _mt5_approvals_day30
@@ -21,6 +21,7 @@ from . import subscriptions as _subscriptions
 from . import trading_controls_day31 as _trading_controls_day31
 from . import user_mt5_accounts as _user_mt5_accounts
 
+_admin_accounts.router.include_router(_member_connection_v2.router)
 _subscriptions.owner_router.include_router(_complimentary_access.router)
 _user_mt5_accounts.router.include_router(_subscriptions.user_router)
 _user_mt5_accounts.router.include_router(_trading_controls_day31.router)
